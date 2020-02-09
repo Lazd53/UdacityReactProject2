95ac21fcf88160c5f12f0bcd1abaff402a0a8976
Alex Love <lazd53@gmail.com>
2020-02-09 14:43:09 -0800

    feat: Game to show change in game mode from questions to leaderboard

--- src/components/GameModeSelector.js
+++ src/components/GameModeSelector.js
@@ -2,20 +2,39 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 class GameModeSelector extends React.Component{
+
+  unClickableLink = (e, thisLocation) => {
+    let {pathName} = this.props.location;
+    if (thisLocation === pathName)
+    e.preventDefault()
+  }
+
   render(){
-    let {pathname} = this.props.location
+    let {pathname} = this.props.location;
+    let [questions, leaderboard] = [false, false];
+    pathname === '/Leaderboard' ?
+      leaderboard = true :
+      questions = true;
+
     return (
       <div className="game-mode-selector">
-        { pathname === "/Leaderboard" ?
-          <Link className="game-mode-selector-link" to="/">
-            Questions
-          </Link> :
-          <Link className="game-mode-selector-link" to="#">
-            Questions
-          </Link>
+        <Link
+          className={questions ?
+            "game-mode-selector-link selected":
+            "game-mode-selector-link"
+          }
+          to="/"
+        >
+          Questions
+        </Link>
 
-        }
-        <Link className="game-mode-selector-link" to="/Leaderboard">
+        <Link
+          className={leaderboard ?
+            "game-mode-selector-link selected":
+            "game-mode-selector-link"
+          }
+          to="/Leaderboard"
+        >
           LeaderBoard
         </Link>
       </div>
