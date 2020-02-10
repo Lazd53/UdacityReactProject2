5b8eb4554889d12e5707b90a7d65e6dc22265f11
Alex Love <lazd53@gmail.com>
2020-02-10 15:04:54 -0800

    feat: Complete page for answered questions

--- src/components/WYRAnswers.js
+++ src/components/WYRAnswers.js
@@ -2,25 +2,39 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {handleSaveQuestionAnswer} from '../actions/shared';
 
-class WYRQuestion extends React.Component {
-
-  chooseOption(answer){
-    let {dispatch, authdUser, currentQuestion} = this.props;
-    dispatch(handleSaveQuestionAnswer(authdUser, currentQuestion.id, answer))
-  }
+class WYRAnswers extends React.Component {
 
   render(){
-    let { currentQuestion, questions } = this.props;
+    let { currentQuestion, questions, authdUser } = this.props;
+    let totalVotes = currentQuestion.optionOne.votes.length + currentQuestion.optionTwo.votes.length;
+    let percentOptionOne = Math.floor(currentQuestion.optionOne.votes.length / totalVotes * 100);
+    let votedForCurrent = authdUser.answers[currentQuestion.id];
+    console.log(votedForCurrent);
     return (
-      <div className="wyr-card-buttons">
-        <h2 className = "wyr-card-button">
-          {currentQuestion.optionOne.text}
-        </h2>
-        <p> OR </p>
-        <h2 className = "wyr-card-button">
-          {currentQuestion.optionTwo.text}
-        </h2>
-
+      <div className="wyr-card-answers">
+        <div className="answer-option">
+          <h3 className="answer-option-title">{currentQuestion.optionOne.text}</h3>
+          <p className="answer-option-number"> {currentQuestion.optionOne.votes.length} {currentQuestion.optionOne.votes.length === 1 ? "vote" : "votes"} </p>
+          {totalVotes > 0 &&
+            <div className="answer-option-percent-circle">
+              <p className="answer-option-percent">{percentOptionOne}%</p>
+            </div>
+          }
+          {votedForCurrent === "optionOne" && <div className="votedFor"><p>You voted for this one!</p></div>}
+        </div>
+        <h4> OR </h4>
+        <div className="answer-option">
+          <h3 className="answer-option-title">{currentQuestion.optionTwo.text}</h3>
+          <p className="answer-option-number"> {currentQuestion.optionTwo.votes.length} {currentQuestion.optionTwo.votes.length === 1 ? "vote" : "votes"} </p>
+          {totalVotes > 0 &&
+            <div className="answer-option-percent-circle">
+              <p className="answer-option-percent">{100 - percentOptionOne}%</p>
+            </div>}
+          {votedForCurrent === "optionTwo" &&
+            <div className="voted-for">
+              <p className="voted-for-text">You voted for this one!</p>
+            </div>}
+        </div>
       </div>
     )
   }
@@ -28,5 +42,6 @@ class WYRQuestion extends React.Component {
 
 export default connect ( (store) => ({
   questions: store.questions,
+  authdUser: store.users[store.authdUser],
   currentQuestion: store.questions[store.currentQuestion]
-}))(WYRQuestion);
+}))(WYRAnswers);
